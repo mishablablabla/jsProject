3fc0d71c4083cb4aa72ad4ce3853e95e57ebebb1
Misha <mzuew2006@gmail.com>
2024-01-16 00:23:40 +0200

    wrote first program

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-const obj = {
-    name : 'Misha',
-    age : 17
-};
+const  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?");
 
-function myFn (person) {
-    const objOne = {...person}; 
-    objOne.age ++;
-    return objOne;
-}
+const personalMovieDB = {
+        count : numberOfFilms,
+        movies : {
 
-const updatedObj = myFn(obj);
+       },
+       actors : {
 
-console.log(obj);
-console.log(updatedObj);
+       },
+       geners : [],
+       privat : false
+    };
+
+ const  a = prompt("Один из последних посмотреных фильмов ?"),
+        b = prompt("На сколько оцените его ?"),
+        c = prompt("Один из последних посмотреных фильмов ?"),
+        d = prompt("На сколько оцените его ?");
+ 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
