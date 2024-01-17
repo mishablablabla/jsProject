6de5874798aff99835a1bb49e1aa8615314842d0
Misha <mzuew2006@gmail.com>
2024-01-17 20:55:13 +0200

    second exersise on cycles

--- main.js
+++ main.js
@@ -8,12 +8,36 @@ const persomalMovieDB = {
         privat : false
 };
 
-const a = prompt('Один из послледних просмотреных фильмов ?'),
-      b = prompt('На сколько оцените его ?'),
-      c = prompt('Один из послледних просмотреных фильмов ?'),
-      d = prompt('На сколько оцените его ?');
+// const a = prompt('Один из послледних просмотреных фильмов ?'),
+//       b = prompt('На сколько оцените его ?'),
+//       c = prompt('Один из послледних просмотреных фильмов ?'),
+//       d = prompt('На сколько оцените его ?');
 
-persomalMovieDB.movies[a] = b;
-persomalMovieDB.movies[c] = d;
+let a, b;
+
+ for (let i = 0; i < 2; i++) {
+        a = prompt('Один из послледних просмотреных фильмов ?');
+        b = prompt('На сколько оцените его ?');
+          if (a.length < 1 || a.length > 50 || a === null || b === null ) {
+                i--;
+                alert('ну ты угарный челик')
+          } else {
+                alert('заебца')
+          }
+        
+        persomalMovieDB.movies[a] = b;
+ }      
+
+
+if ( persomalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов !')
+} else if ( persomalMovieDB.count > 10 && persomalMovieDB.count < 30 ) {
+        alert('Вы класический зритель ')   
+} else if ( persomalMovieDB.count > 30 ) {
+        alert('Вы киноман')   
+} else {
+        alert('Произошла ошибка!')
+}
+
 
 console.log(persomalMovieDB)
