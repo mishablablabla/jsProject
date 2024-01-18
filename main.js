e71b43292adbf6b2dcfff09b7b58bf4542ae7edb
Misha <mzuew2006@gmail.com>
2024-01-18 02:24:03 +0200

    practice №3

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+let numberOfFilms 
+
+function start() {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+                numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+        }
+}
+
+//start();
 
 const persomalMovieDB = {
         count : numberOfFilms,
@@ -8,36 +18,53 @@ const persomalMovieDB = {
         privat : false
 };
 
-// const a = prompt('Один из послледних просмотреных фильмов ?'),
-//       b = prompt('На сколько оцените его ?'),
-//       c = prompt('Один из послледних просмотреных фильмов ?'),
-//       d = prompt('На сколько оцените его ?');
-
-let a, b;
-
- for (let i = 0; i < 2; i++) {
-        a = prompt('Один из послледних просмотреных фильмов ?');
-        b = prompt('На сколько оцените его ?');
-          if (a.length < 1 || a.length > 50 || a === null || b === null ) {
-                i--;
-                alert('ну ты угарный челик')
-          } else {
-                alert('заебца')
-          }
-        
-        persomalMovieDB.movies[a] = b;
- }      
 
 
-if ( persomalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов !')
-} else if ( persomalMovieDB.count > 10 && persomalMovieDB.count < 30 ) {
-        alert('Вы класический зритель ')   
-} else if ( persomalMovieDB.count > 30 ) {
-        alert('Вы киноман')   
-} else {
-        alert('Произошла ошибка!')
+ function rememberMyFilms () {
+        for (let i = 0; i < 2; i++) {
+            const   a = prompt('Один из послледних просмотреных фильмов ?'),
+                    b = prompt('На сколько оцените его ?');
+
+                 if (a.length < 1 || a.length > 50 || a === null || b === null ) {
+                       i--;
+                       alert('ну ты угарный челик')
+                 } else {
+                       alert('заебца')
+                 }
+               persomalMovieDB.movies[a] = b;
+        }      
+ }
+
+ //rememberMyFilms();
+
+function detectPersonalLevel () {
+        if ( persomalMovieDB.count < 10) {
+                alert('Просмотрено довольно мало фильмов !')
+        } else if ( persomalMovieDB.count > 10 && persomalMovieDB.count < 30 ) {
+                alert('Вы класический зритель ')   
+        } else if ( persomalMovieDB.count > 30 ) {
+                alert('Вы киноман')   
+        } else {
+                alert('Произошла ошибка!')
+        };
+};
+
+//detectPersonalLevel();
+
+function showMyDB () {
+        if (persomalMovieDB.privat === false) {
+                console.log(persomalMovieDB)
+        };
+};
+
+//showMyDB();
+
+function writeYourGeneres () {
+        for (let i = 1; i <= 3; i++) {
+          persomalMovieDB.geners[i - 1] = prompt(`Ваш любимый жанр под номером ${i} `);
+        } 
 }
-
+writeYourGeneres();
 
 console.log(persomalMovieDB)
+console.log(persomalMovieDB.geners)
